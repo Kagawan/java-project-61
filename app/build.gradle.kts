2c86de1538e88cab1fc6cb9ae10607f1700e93d3
rashid <rashid060803@mail.ru>
2024-05-15 18:01:44 +0300

    build.gradle.kts deleted github.ben-manes,johnrengelman

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,8 +6,6 @@ plugins {
     jacoco
     id("checkstyle")
     id("io.freefair.lombok") version "8.4"
-    id("com.github.ben-manes.versions") version "0.50.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "hexlet.code.games"
